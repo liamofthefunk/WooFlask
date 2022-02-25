3090a60db38737073324093e1d946da6f0564c3c
liam shortall <liamofthefunk@gmail.com>
2022-02-25 10:25:54 +0000

    flask push

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from logging import exception
+import logging
+from flask import Flask, render_template
+from flask import request
+import pika
+from website import create_app
+
+
+app = create_app()
+@app.route('/', methods=['GET','POST'])
+def webhook():
+    if request.headers['Content-Type'] == 'application/json':
+        data = request.json  
+        try:
+
+            connection = pika.BlockingConnection(pika.URLParameters(
+                'amqps://ksljhpzg:b-GA2Sl_VIivj8zYUR0h9oFopdQRwR8Q@rattlesnake.rmq.cloudamqp.com/ksljhpzg'))
+            channel = connection.channel()
+            channel.queue_declare(queue='wc_orders-in')
+            channel.basic_publish(exchange='router',
+                                routing_key='orders',
+                                body=json.dumps(data))
+            connection.close()
+        except Exception as Argument:
+            logging.exception('Error!')
+    
+        return 'OK', 200
+
+if __name__ == '__main__':
+    app.run()
